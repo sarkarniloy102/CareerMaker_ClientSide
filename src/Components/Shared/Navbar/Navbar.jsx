2377a0bb87f8ba1fa51e9b404f56fc5aa64f484c
sarkar niloy <sarkarniloy102@gmail.com>
2023-11-05 04:55:15 +0600

    create footer section

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
                         {navbar}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl hover:text-purple-400">Career Maker</a>
+                <a className="btn btn-ghost normal-case text-xl hover:text-purple-400">Career maker</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
